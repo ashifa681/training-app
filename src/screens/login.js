30c5a279466218e45dbae22bcecac0560e296c21
ashifa Eman <riazmahar198@gmail.com>
2025-06-27 22:56:00 +0500

    features folders of screens etc added in src

--- src/screens/login.js
+++ src/screens/login.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function app() {
+export default function login() {
   return (
     <View style={styles.container}>
       <Text>Hello my first app git configuration done, Congredullabtions</Text>
